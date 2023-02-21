d1dab558f4f09f0e338e32d0055b66de614941e0
jitujiten <sahuohjitu@gmail.com>
2023-02-21 10:56:59 +0530

    Added signup and login page

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import AuthForm from "./component/AuthForm";
+
+
+function App() {
+  return (
+   <div className="container">
+   <AuthForm></AuthForm>
+   </div>
+  );
+}
+
+export default App;
